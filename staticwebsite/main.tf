9b62005c68a95e944ea037e3f2fafa3451b6ba0e
Al Nwokedike <al@alnwoks.com>
2022-12-11 13:18:52 +0100

    update config in repo

--- staticwebsite/main.tf
+++ staticwebsite/main.tf
@@ -2,22 +2,24 @@ provider "aws" {
   region  = var.aws_region
 }
  
+data "aws_caller_identity" "current" {}
+
 # You cannot create a new backend by simply defining this and then
 # immediately proceeding to "terraform apply". The S3 backend must
 # be bootstrapped according to the simple yet essential procedure in
 # https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
 module "terraform_state_backend" {
-  source = "cloudposse/tfstate-backend/aws"
-  # Cloud Posse recommends pinning every module to a specific version
-  # version     = "x.x.x"
+  source     = "cloudposse/tfstate-backend/aws"
+  version    = "0.38.1"
   namespace  = "alnwoks"
-  stage      = "infra"
-  name       = "terraform"
-  attributes = ["state"]
+  name       = "tfstate"
+  attributes = [data.aws_caller_identity.current.account_id]
 
-  terraform_backend_config_file_path = "."
+  terraform_backend_config_file_path = ""
   terraform_backend_config_file_name = "backend.tf"
-  force_destroy                      = false
+
+  terraform_state_file = "staticwebsite/terraform.tfstate"
+  force_destroy        = false
 }
 
 module "staticwebsite-s3" {
